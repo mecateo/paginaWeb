8697eb170c0052b020108e5b5d85596fc7f5ddff
Miguel Angel Meca Pacherres <miguel_meca01@hotmail.com>
2019-09-11 10:31:49 -0500

    Se agrego un boton cancelar El botón que se agreo es el de Cancelar en el archivo index.html Luego se programó su evento en javascript para que nos muestre el mensaje de que se ha presionado el boton CANCELAR

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 (function(){
     let boton = document.getElementById('js-buttonSay');
+    let cancelButton = document.getElementById('js-buttonCancelar');
     boton.addEventListener('click', function(){
-        alert('Bienvenidos a mi practica de git con un proyecto real');
+        alert('Bienvenidos a mi practica de git con un proyecto real.');
+    });
+    cancelButton.addEventListener('click', function(){
+        alert('Ha hecho click en el botón Cancelar.');
     });
 })();
