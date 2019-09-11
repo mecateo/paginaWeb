120ec68ef4c276dc01c3877aac0e957235c5f01d
Miguel Angel Meca Pacherres <miguel_meca01@hotmail.com>
2019-09-10 21:48:03 -0500

    Estructura inciala de la pagina Se agrego un archivo index.html y script.js el archivo index contiene un boton que al hacer clic sobre el nos muestra un saludo

---
+++ script.js
@@ -0,0 +1,6 @@
+(function(){
+    let boton = document.getElementById('js-buttonSay');
+    boton.addEventListener('click', function(){
+        alert('Bienvenidos a mi practica de git con un proyecto real');
+    });
+})();
